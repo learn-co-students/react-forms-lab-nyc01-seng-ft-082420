7afc0a276e8cedc429e76edef58abcee6bbdd839
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-28 18:54:03 -0400

    working with controlled form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,41 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+
+  handleField = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  login = (event) => {
+    event.preventDefault()
+    if (this.state.username.length > 1 && this.state.password.length > 1) {
+      this.props.handleLogin(this.state.username, this.state.password)
+
+    }
+    
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => {this.login(e)}}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleField} id="username" name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handleField} mid="password" name="password" type="password" value={this.state.password} />
           </label>
         </div>
         <div>
